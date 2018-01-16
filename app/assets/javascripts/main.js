d61b79e5dfe0cb020073d91aeb755ddd09bc6a10
bob <bogdan4uk@gmail.com>
2018-01-16 17:31:26 +0200

    cart in progress

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -11,10 +11,51 @@
     // Listen for the jQuery ready event on the document
     $(function() {
 
-        console.log('dom iz redi');
-
         // initialize nice select
         $('select').niceSelect();
+
+        // show/hide cart
+        $('.cart-icon').on('click', function(){
+            if ($('.cart-container').hasClass('visible')){
+                $('.cart-container').removeClass('visible');
+                $('body').removeClass('overflow-hidden');
+            } else {
+                $('.cart-container').addClass('visible');
+                $('body').addClass('overflow-hidden');
+            }
+        })
+        $('.cart-close-btn').on('click', function(){
+            $('.cart-container').removeClass('visible');
+            $('body').removeClass('overflow-hidden');
+        })
+        // close cart
+        $.clickOut('.cart-container',
+            function(){
+                $('.cart-container').removeClass('visible');
+                $('body').removeClass('overflow-hidden');
+            },{except: '.cart-container, .cart-icon'}
+        )
+
+
+        // show contact box
+        $('.contact-dropdown-btn').on('click', function(){
+            if ($('.contact-dropdown').hasClass('visible')){
+                $('.contact-dropdown').removeClass('visible');
+                $(this).removeClass('active');
+            } else {
+                $('.contact-dropdown').addClass('visible');
+                $(this).addClass('active');
+            }
+        })
+        // close cart
+        $.clickOut('.contact-dropdown',
+            function(){
+                $('.contact-dropdown').removeClass('visible');
+                $('.contact-dropdown-btn').removeClass('active');
+            },{except: '.contact-dropdown, .contact-dropdown-btn'}
+        )
+
+
 
     });
 
