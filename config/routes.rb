aca19b4eedbeebf809b642c49ac0062e67d89fe1
bob <bogdan4uk@gmail.com>
2018-01-23 18:12:23 +0200

    constructor started

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   controller :pages do
 
     get "structure-parts", action: "structure_parts"
+    get "constructor", action: "constructor"
 
     get "drinks", action: "drinks"
     get "promotions", action: "promotions"
