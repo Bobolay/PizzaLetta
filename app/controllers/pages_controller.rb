9871c0dcc72399c2f6bb048d3b15511cf5b5a714
Victor Odarchenko <vodarchenko@gmail.com>
2018-03-29 16:02:42 +0300

    BODYA TU DED

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -83,8 +83,7 @@ class PagesController < ApplicationController
       list = Orderlist.new
       list.name = s[:name]
       list.quantity = s[:qnty]
-      list.price = s[:qnty] * s[:pricesmall]
-      binding.pry
+      list.price = s[:qnty].to_i * s[:price].to_i
       if s[:bonus]
       list.bonus_name = s[:bonus][:name]
       list.bonus_description = s[:bonus][:attribute]
