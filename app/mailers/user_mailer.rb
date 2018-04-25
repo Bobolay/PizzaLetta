6ec4b61e5d4232863840a671792ebef592dc2130
bob <bogdan4uk@gmail.com>
2018-04-25 15:43:19 +0300

    text-text-tex

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,15 +4,15 @@ class UserMailer < ApplicationMailer
  def order_email(order)
    @array=order
    @list= order[:cart]
-   mail(to: "pizzaletta17@gmail.com", subject: 'замовлення')
+   mail(to: "yuriy.h@voroninstudio.eu", subject: 'замовлення')
  end
  def subscribe_email(email)
    @email = email
-   mail(to: "pizzaletta17@gmail.com", subject: 'замовлення')
+   mail(to: "yuriy.h@voroninstudio.eu", subject: 'замовлення')
  end
  def call_email(call)
    @call=call
-   mail(to: "pizzaletta17@gmail.com", subject: 'замовлення')
+   mail(to: "yuriy.h@voroninstudio.eu", subject: 'замовлення')
  end
 
 end
