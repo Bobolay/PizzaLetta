1b5c819f2056bb3d20fa65ad51921af472899216
bob <bogdan4uk@gmail.com>
2018-05-17 11:14:35 +0300

    mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
  def order_email(order)
    @array=order
    @list= order[:cart]
-   mail(to: "pizzaletta17@gmail.com", subject: 'замовлення')
+   mail(to: "yuriy.h@voroninstudio.eu", subject: 'замовлення')
  end
  def subscribe_email(email)
    @email = email
