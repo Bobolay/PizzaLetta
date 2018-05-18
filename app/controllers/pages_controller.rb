4a04598ef050d44bad73673f6f0e66799bc65887
Victor Odarchenko <vodarchenko@gmail.com>
2018-05-18 12:42:05 +0300

    mobidel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -64,7 +64,6 @@ class PagesController < ApplicationController
         else
           url = url + "&comment=#{params[:info][:comment].parameterize}"
         end
-        #binding.pry
     elsif  params[:info][:orderway] == "Забрати самому"
       timing = params[:info][:time] + " " + params[:info][:date]
       url = "http://online.mobidel.ru/makeOrder.php?%20user=internet&password=casper12345&wid=7021&phone=#{params[:info][:phone]}&family=#{params[:info][:name].parameterize}&advanceDeliveryDate=#{timing}&independently=1&warehouseID=855983713182099675"
@@ -148,7 +147,7 @@ class PagesController < ApplicationController
       list.order_id = @order.id
       list.save
     end
-    # HTTParty.get(url)
+    HTTParty.get(url)
     if @order.save
       UserMailer.order_email(@array).deliver_now
     end
